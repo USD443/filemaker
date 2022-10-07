ee51c06fc3dd7c45fff6b4c74409ed417aeca27f
carlosrrdev <rr.carlos.dev@gmail.com>
2022-10-07 09:21:15 -0500

    config router

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import { MainMenu } from "./routes/MainMenu";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <MainMenu />,
-  },
-]);
-
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <Router>
+      <Routes>
+        <Route path="/" element={<MainMenu />} />
+      </Routes>
+    </Router>
   </React.StrictMode>
 );
